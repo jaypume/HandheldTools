675d928ccd13bb2411639e8e85be5842d331407e
Jie Pu <i@jaypu.com>
2023-02-12 19:09:28 +0800

    init

---
+++ App.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Forms = System.Windows.Forms;
+
+namespace HandheldTools
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly Forms.NotifyIcon _notifyIcon;
+        private MainWindow _mainWindow;
+
+        public App()
+        {
+            _notifyIcon = new Forms.NotifyIcon();
+            _mainWindow = new MainWindow();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            //MainWindow.Left = SystemParameters.PrimaryScreenWidth - MainWindow.Width;
+            //MainWindow.Show();
+            //_mainWindow.Activate();
+            //_mainWindow.ShowDialog();
+
+            _notifyIcon.Icon = new System.Drawing.Icon("resources/main.ico");
+            _notifyIcon.Text = "掌机控制小工具";
+            _notifyIcon.Click += NotifyIconClick;
+
+            _notifyIcon.ContextMenuStrip = new Forms.ContextMenuStrip();
+            _notifyIcon.ContextMenuStrip.Items.Add("Status", Image.FromFile("resources/main.ico"), onStatusClicked);
+            _notifyIcon.ContextMenuStrip.Items.Add("Exit", Image.FromFile("resources/main.ico"), onExitClicked);
+
+            _notifyIcon.Visible= true; 
+
+            base.OnStartup(e);
+        }
+
+        private void onExitClicked(object? sender, EventArgs e)
+        {
+            //MainWindow.WindowState = WindowState.Normal;
+
+            // show 和 active是有区别的：
+            _mainWindow.WindowState = WindowState.Normal;
+            _mainWindow.Activate();
+        }
+
+        private void onStatusClicked(object? sender, EventArgs e)
+        {
+            MessageBox.Show("Application is running", "Status", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void NotifyIconClick(object? sender, EventArgs e)
+        {
+            _mainWindow.WindowState= WindowState.Normal;
+            _mainWindow.Activate();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            //_notifyIcon.Dispose();
+            //_mainWindow.Close();
+            base.OnExit(e);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HandheldTools
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //InitializeComponent();
+            
+
+        }
+
+
+    }
+
+   
+}
+
+
+namespace HandheldTools.Tools
+{
+
+    [ValueConversion(typeof(string), typeof(string))]
+    public class RatioConverter : MarkupExtension, IValueConverter
+    {
+        private static RatioConverter _instance;
+
+        public RatioConverter() { }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        { // do not let the culture default to local to prevent variable outcome re decimal syntax
+            double size = System.Convert.ToDouble(value) * System.Convert.ToDouble(parameter, CultureInfo.InvariantCulture);
+            return size.ToString("G0", CultureInfo.InvariantCulture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        { // read only converter...
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _instance ?? (_instance = new RatioConverter());
+        }
+
+    }
+}
